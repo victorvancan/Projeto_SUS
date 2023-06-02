af5c2e508789b4d72b935cc141c8f9fd83988164
victor vançan <74362179+victorvancan@users.noreply.github.com>
2023-06-02 16:49:14 -0300

    Barra de pesquisa em JS

---
+++ js/sugestao.js
@@ -0,0 +1,15 @@
+let suggestions = [
+    "Cardiologia",
+    "Dermatologia",
+    "endocrinolgia",
+    "endocrinolgia",
+    "neurologia",
+    "geraitria",
+    "Oftalmologia",
+    "Oncologia",
+    "Ortopedia",
+    "Pediatria",
+    "Saude da mulher",
+    "SUS",
+    "Exames"
+];
